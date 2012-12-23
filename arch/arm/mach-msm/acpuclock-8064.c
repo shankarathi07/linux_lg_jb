4251585a9b9e645f862b965d2bd5f88e717a0b17
Shankar <mathkid95@gmail.com>
2012-12-23 10:13:17 -0600

    Add a 288 Mhz CPU step and remove the 432 Mhz step
    After testing this CPU step it was found the phone had better battery usage when idle and went into deep sleep more, using the 288 Mhz as the standby speed.

--- arch/arm/mach-msm/acpuclock-8064.c
+++ arch/arm/mach-msm/acpuclock-8064.c
@@ -106,8 +106,8 @@ static struct msm_bus_scale_pdata bus_scale_data __initdata = {
 };
 
 static struct l2_level l2_freq_tbl[] __initdata __initdata = {
-	[0]  = { {  384000, PLL_8, 0, 2, 0x00 }, 950000, 950000, 1 },
-	[1]  = { {  432000, HFPLL, 2, 0, 0x20 }, 950000, 950000, 2 },
+    [0]  = { {  288000, PLL_8, 0, 2, 0x00 }, 950000, 950000, 1 },
+	[1]  = { {  384000, PLL_8, 0, 2, 0x20 }, 950000, 950000, 1 },
 	[2]  = { {  486000, HFPLL, 2, 0, 0x24 }, 950000, 950000, 2 },
 	[3]  = { {  540000, HFPLL, 2, 0, 0x28 }, 950000, 950000, 2 },
 	[4]  = { {  594000, HFPLL, 1, 0, 0x16 }, 950000, 950000, 2 },
@@ -125,8 +125,8 @@ static struct l2_level l2_freq_tbl[] __initdata __initdata = {
 };
 
 static struct acpu_level acpu_freq_tbl_slow[] __initdata = {
-	{ 1, {   384000, PLL_8, 0, 2, 0x00 }, L2(0),   850000 },
-	{ 0, {   432000, HFPLL, 2, 0, 0x20 }, L2(6),   875000 },
+    { 1, {   288000, PLL_8, 0, 2, 0x00 }, L2(0),   850000 },
+	{ 0, {   384000, PLL_8, 0, 2, 0x20 }, L2(6),   875000 },
 	{ 1, {   486000, HFPLL, 2, 0, 0x24 }, L2(6),   875000 },
 	{ 0, {   540000, HFPLL, 2, 0, 0x28 }, L2(6),   900000 },
 	{ 1, {   594000, HFPLL, 1, 0, 0x16 }, L2(6),   900000 },
@@ -151,8 +151,8 @@ static struct acpu_level acpu_freq_tbl_slow[] __initdata = {
 };
 
 static struct acpu_level acpu_freq_tbl_nom[] __initdata = {
-	{ 1, {   384000, PLL_8, 0, 2, 0x00 }, L2(0),   800000 },
-	{ 0, {   432000, HFPLL, 2, 0, 0x20 }, L2(6),   825000 },
+    { 1, {   288000, PLL_8, 0, 2, 0x00 }, L2(0),   800000 },
+	{ 0, {   384000, PLL_8, 0, 2, 0x20 }, L2(6),   825000 },
 	{ 1, {   486000, HFPLL, 2, 0, 0x24 }, L2(6),   825000 },
 	{ 0, {   540000, HFPLL, 2, 0, 0x28 }, L2(6),   850000 },
 	{ 1, {   594000, HFPLL, 1, 0, 0x16 }, L2(6),   850000 },
@@ -177,8 +177,8 @@ static struct acpu_level acpu_freq_tbl_nom[] __initdata = {
 };
 
 static struct acpu_level acpu_freq_tbl_fast[] __initdata = {
-	{ 1, {   384000, PLL_8, 0, 2, 0x00 }, L2(0),   750000 },
-	{ 0, {   432000, HFPLL, 2, 0, 0x20 }, L2(6),   775000 },
+    { 1, {   288000, PLL_8, 0, 2, 0x00 }, L2(0),   750000 },
+	{ 0, {   384000, PLL_8, 0, 2, 0x20 }, L2(6),   775000 },
 	{ 1, {   486000, HFPLL, 2, 0, 0x24 }, L2(6),   775000 },
 	{ 0, {   540000, HFPLL, 2, 0, 0x28 }, L2(6),   800000 },
 	{ 1, {   594000, HFPLL, 1, 0, 0x16 }, L2(6),   800000 },
@@ -202,9 +202,9 @@ static struct acpu_level acpu_freq_tbl_fast[] __initdata = {
 	{ 0, { 0 } }
 };
 
-static struct acpu_level tbl_faster[] __initdata = {
-    { 1, {   384000, PLL_8, 0, 0x00 }, L2(0),   750000 },
-    { 0, {   432000, HFPLL, 2, 0x20 }, L2(6),   775000 },
+static struct acpu_level acpu_freq_tbl_faster[] __initdata = {
+    { 1, {   288000, PLL_8, 0, 0x00 }, L2(0),   750000 },
+    { 0, {   384000, PLL_8, 0, 0x20 }, L2(6),   775000 },
     { 1, {   486000, HFPLL, 2, 0x24 }, L2(6),   775000 },
     { 0, {   540000, HFPLL, 2, 0x28 }, L2(6),   800000 },
     { 1, {   594000, HFPLL, 1, 0x16 }, L2(6),   800000 },
@@ -244,7 +244,7 @@ static struct acpuclk_krait_params acpuclk_8064_params __initdata = {
 	.l2_freq_tbl_size = sizeof(l2_freq_tbl),
 	.bus_scale = &bus_scale_data,
 	.qfprom_phys_base = 0x00700000,
-	.stby_khz = 384000,
+	.stby_khz = 288000,
 };
 
 static int __init acpuclk_8064_probe(struct platform_device *pdev)
