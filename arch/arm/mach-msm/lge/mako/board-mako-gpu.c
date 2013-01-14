b40ad3b9052a8aaebcab7d3f31c5e1bae489ed51
Shankar <mathkid95@gmail.com>
2013-01-14 17:01:52 -0600

    Revert "450 Mhz GPU OC"
    This reverts commit 52640b2892f92c0d314bee8ff892b3f7a111593d.

--- arch/arm/mach-msm/lge/mako/board-mako-gpu.c
+++ arch/arm/mach-msm/lge/mako/board-mako-gpu.c
@@ -196,15 +196,15 @@ static struct kgsl_device_iommu_data kgsl_3d0_iommu_data[] = {
 
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
-        {
-			.gpu_freq = 450000000,
-			.bus_freq = 4,
-			.io_fraction = 0,
-		},
 		{
 			.gpu_freq = 400000000,
 			.bus_freq = 4,
 			.io_fraction = 0,
+		},
+		{
+			.gpu_freq = 325000000,
+			.bus_freq = 3,
+			.io_fraction = 33,
 		},
 		{
 			.gpu_freq = 200000000,
@@ -220,7 +220,6 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 			.gpu_freq = 27000000,
 			.bus_freq = 0,
 		},
-        
 	},
 	.init_level = 1,
 	.num_levels = 5,
@@ -237,7 +236,6 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 #ifdef CONFIG_MSM_DCVS
 	.core_info = &grp3d_core_info,
 #endif
-    
 };
 
 struct platform_device device_kgsl_3d0 = {
